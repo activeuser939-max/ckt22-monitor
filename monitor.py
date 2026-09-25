c1d54f6e3735a54a4c129f38920347e05656715a
activeuser939-max <activeuser939@gmail.com>
2026-09-25 09:33:27 +0900

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,36 @@
+import requests
+
+TOPIC = "ckt22-ayaz-final-7382"
+
+URL = (
+    "https://generalsale.tickets-aichi-nagoya2026.org/"
+    "showProduct.html?idProduct=492"
+)
+
+html = requests.get(
+    URL,
+    headers={
+        "User-Agent":
+        "Mozilla/5.0"
+    }
+).text
+
+available = (
+    "productItem_3797" in html
+    and
+    'pointer-events: none;" title="Increase" class="productPlus'
+    not in html
+)
+
+if available:
+
+    requests.post(
+        f"https://ntfy.sh/{TOPIC}",
+        data="🏏 CKT22 Category A General Available!"
+    )
+
+    print("AVAILABLE")
+
+else:
+
+    print("NOT AVAILABLE")
